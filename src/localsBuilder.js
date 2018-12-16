5957d2fabd79d1d9793a0b8443409da24e19fd02
Kaenguru-Knobel-Kette <manuel.nowack@gmail.com>
2018-12-16 15:46:24 +0100

    Simplify building locals

--- src/localsBuilder.js
+++ src/localsBuilder.js
@@ -1,18 +1,15 @@
 localsBuilder = {
 	setLocals: function(patch, helpers, settings, locals) {
-		let aos = xelib.FileByName('AOS.esp');
 		let skyrim = xelib.FileByName('Skyrim.esm');
+		let aos = xelib.FileByName('AOS.esp');
 
-		let daggerImpact = xelib.GetRecord(aos, 0x0405B6EA);
-		let daggerEquip = xelib.GetRecord(skyrim, 0x0003CED9);
-		let daggerUnequip = xelib.GetRecord(skyrim, 0x0003CEDB);
-		let bluntImpact = xelib.GetRecord(skyrim, 0x000193C7);
-		let giantClubImpact = xelib.GetRecord(skyrim, 0x000C4E32);
-		
-		locals.daggerImpact = xelib.GetHexFormID(daggerImpact, false, false);
-		locals.daggerEquip = xelib.GetHexFormID(daggerEquip, false, false);
-		locals.daggerUnequip = xelib.GetHexFormID(daggerUnequip, false, false);
-		locals.bluntImpact = xelib.GetHexFormID(bluntImpact, false, false);
-		locals.giantClubImpact = xelib.GetHexFormID(giantClubImpact, false, false);
+		let skyrimIndex = xelib.Hex(xelib.GetFileLoadOrder(skyrim), 2);
+		let aosIndex = xelib.Hex(xelib.GetFileLoadOrder(aos), 2);
+
+		locals.daggerImpact = `${aosIndex}05B6EA`;
+		locals.daggerEquip = `${skyrimIndex}03CED9`;
+		locals.daggerUnequip = `${skyrimIndex}03CEDB`;
+		locals.bluntImpact = `${skyrimIndex}0193C7`;
+		locals.giantClubImpact = `${skyrimIndex}0C4E32`;
 	}
 }
