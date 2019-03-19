721e7f91a75ed17ecdcf0db03e3e960c18bbe7e7
Kaenguru-Knobel-Kette <manuel.nowack@gmail.com>
2019-03-20 00:04:23 +0100

    Remove localsBuilder module

--- src/localsBuilder.js
+++ src/localsBuilder.js
@@ -1,15 +1,13 @@
-localsBuilder = {
-	setLocals: function(patch, helpers, settings, locals) {
-		let skyrim = xelib.FileByName('Skyrim.esm');
-		let aos = xelib.FileByName('AOS.esp');
+function setLocals(patch, helpers, settings, locals) {
+	let skyrim = xelib.FileByName('Skyrim.esm');
+	let aos = xelib.FileByName('AOS.esp');
 
-		let skyrimIndex = xelib.Hex(xelib.GetFileLoadOrder(skyrim), 2);
-		let aosIndex = xelib.Hex(xelib.GetFileLoadOrder(aos), 2);
+	let skyrimIndex = xelib.Hex(xelib.GetFileLoadOrder(skyrim), 2);
+	let aosIndex = xelib.Hex(xelib.GetFileLoadOrder(aos), 2);
 
-		locals.daggerImpact = `${aosIndex}05B6EA`;
-		locals.daggerEquip = `${skyrimIndex}03CED9`;
-		locals.daggerUnequip = `${skyrimIndex}03CEDB`;
-		locals.bluntImpact = `${skyrimIndex}0193C7`;
-		locals.giantClubImpact = `${skyrimIndex}0C4E32`;
-	}
+	locals.daggerImpact = `${aosIndex}05B6EA`;
+	locals.daggerEquip = `${skyrimIndex}03CED9`;
+	locals.daggerUnequip = `${skyrimIndex}03CEDB`;
+	locals.bluntImpact = `${skyrimIndex}0193C7`;
+	locals.giantClubImpact = `${skyrimIndex}0C4E32`;
 }
